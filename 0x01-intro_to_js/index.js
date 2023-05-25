29db9be3e3d6ee7b6b51248b5d7615ca5b4e70da
wendy <wendyagunu200@gmail.com>
2023-05-25 23:48:10 +0000

    working directory

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,12 @@
+let myName ="wendy";
+
+
+let  myName ="Agunu";
+
+let car = {
+
+brand:  "Toyota"
+Numb.Plate:   108654
+red color:  false
+
+};
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,22 @@
+const  favoriteNumber = 10;
+
+const  favoriteNumber =10;
+const half =2;
+const  halfOfFavNum = a/b;
+const  halfOfFavNum =5;
+
+let  isaacsFavorite = 13;
+
+isaacsFavorite -  favoriteNumber = 3;
+
+let  isaacsFavorite = 26 ;
+let times = 13;
+let value =  isaacsFavorite*times;
+value =338;
+
+let a =26;
+let b = 3;
+let isaacsFavorite = a/b;
+isaacsFavorite =8.6666666666666666666666666666667;
+
+
